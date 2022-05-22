deb4cba4d2dd41c7fb49c4835d54191cb91a412d
Aravind Sundaresan <asundaresan@gmail.com>
2022-05-22 16:07:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,32 @@
 
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read()
+
 setup(name='import_media',
       version='0.0.1dev',
-      description='Python utilities to import and organize media',
+      description='Python utilities to import and organize media files, specifically photos.',
       url='http://github.com/asundaresan/import_media',
       author='Aravind Sundaresan',
       author_email='asundaresan@gmail.com',
       license='GPLv3',
       packages=find_packages(),
-      scripts=[
-          "bin/import_files.py",
-          "bin/get_metadata.py",
-        ],
+      #package_data={
+      #    'import_data': [
+      #        'data/*/*.h5', 
+      #        ]
+      #    },
+      #scripts=[
+      #  "bin/import_files.py",
+      #  "bin/get_metadata.py",
+      #  ],
       zip_safe=False,
-      install_requires = [
-          "exifread",
-          "tqdm",
-        ]
+      install_requires = requirements,
+      entry_points={
+          'console_scripts': [
+              'import_files=import_media.import_files:main',
+              ],
+          },
       )
 
